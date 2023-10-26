9a85cdc0893f9acf6ca5acb437b51afda460f43a
Sujal-Shresth <sujshrest@gmail.com>
2023-10-26 14:37:06 +0530

    Project Completed

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+// array to list the task
 var theList =[];
 
 var task = document.getElementById("input");
 const button = document.getElementById("button");
 const list = document.getElementById("todolist");
 
+// submit function
 button.onclick = () => {
     if(task.value){
     theList.push(task.value);
@@ -12,6 +14,7 @@ button.onclick = () => {
     }
 }
 
+// display list function
 function display(){
     list.innerHTML = "";
     theList.forEach((task,index) => {
@@ -19,12 +22,15 @@ function display(){
       "<li>"+task+ "<span onclick='edit("+index+")'>Edit&nbsp;&nbsp;</span> <span onclick='remove("+index+")'>X | </span></li>";
     }) 
 }
+
+// edit task function
 function edit(index) {
     var newTask = prompt("Please insert your new value");
     theList.splice(index, 1, newTask);
     display();
 }
 
+//remove task function
 function remove(index) {
     theList.splice(index, 1);
     display();
